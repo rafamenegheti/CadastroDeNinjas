abf9f99b8c3da6f0bccfbe0a38665a7a88a7ca5e
Rafael Menegheti <rafaelmenegheti51@gmail.com>
2025-09-10 16:19:33 -0300

    chore: criando a controller

---
+++ src/main/java/com/estudo/CadastroDeNinjas/Controller.java
@@ -0,0 +1,16 @@
+package com.estudo.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas(){
+        return "Bem-vindo ao Cadastro de Ninjas";
+    }
+
+}
